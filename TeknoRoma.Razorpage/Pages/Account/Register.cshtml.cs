abff6354bc90ba1921ec464ca18c9934901e85da
ataisyar16 <ataisyar59@gmail.com>
2025-01-02 16:20:15 +0300

    Teknoroma

--- TeknoRoma.Razorpage/Pages/Account/Register.cshtml.cs
+++ TeknoRoma.Razorpage/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using TeknoRoma.Entities.Entities.Concrete;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
 
 namespace TeknoRoma.Razorpage.Pages.Account
 {
@@ -15,7 +16,7 @@ namespace TeknoRoma.Razorpage.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [System.ComponentModel.DataAnnotations.Required]
             public string FullName { get; set; }
 
             [Required]
